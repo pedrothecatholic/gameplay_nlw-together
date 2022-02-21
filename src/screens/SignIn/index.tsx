8597d674c7aeddf5b91080350e27e84b096eb532
Pedro Henrique de Sousa <pedrozica.phs21@gmail.com>
2022-02-21 09:12:22 -0300

    aula01 finalizada

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,23 +1,40 @@
-import React, {useState} from "react";
-import { View, Text, TextInput } from "react-native";
+import React from "react";
+import { View, Text, Image, StatusBar } from "react-native";
+
+import { ButtonIcon } from "../../components/ButtonIcon";
+import IllustrationImg from '../../assets/illustration.png';
 import {styles} from './styles';
 
 export function SignIn() {
-    const [text, setText] = useState('');
-
-
     return (
         <View style={styles.container}>
-            <Text>Hello World, NLW Together</Text>
-
-            <TextInput 
-            style={styles.input} 
-            onChangeText={setText}
+            <StatusBar
+                barStyle="light-content"
+                backgroundColor="transparent"
+                translucent
             />
 
-            <Text>
-                Você digitou ... {text}
+            <Image 
+                source={IllustrationImg}
+                style={styles.image}
+                resizeMode="stretch"/>
+
+            <View style={styles.content}>
+                <Text style={styles.title}>
+                    Organize {`\n`}
+                    suas jogatinas {`\n`}
+                    facilmente
+                </Text>
+
+            <Text style={styles.subtitle}>
+                Crie grupos para jogar seus games {`\n`}
+                favoritos com seus amigos
             </Text>
+
+            <ButtonIcon 
+                title="Entrar com Discord"
+                activeOpacity={.7}/>
         </View>
+    </View>
     );
 }
